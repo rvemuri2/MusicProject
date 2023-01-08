651401a7755658febd121fd313e3eb148659fa10
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-08 02:18:45 -0800

    using getters

--- MusicProject/src/components/Auth.vue
+++ MusicProject/src/components/Auth.vue
@@ -1,6 +1,10 @@
 <template>
   <!-- Auth Modal -->
-  <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+  <div
+    class="fixed z-10 inset-0 overflow-y-auto"
+    id="modal"
+    :class="hiddenClass"
+  >
     <div
       class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
@@ -149,7 +153,12 @@
 </template>
 
 <script>
+import useModalStore from "@/stores/modal";
+import { mapState } from "pinia";
 export default {
   name: "Auth",
+  computed: {
+    ...mapState(useModalStore, ["hiddenClass"]),
+  },
 };
 </script>
