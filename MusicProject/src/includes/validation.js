dcdaa584d39c42bcce4fad90ba9d190bd8f3cb84
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-08 23:52:51 -0800

    additional rules

--- MusicProject/src/includes/validation.js
+++ MusicProject/src/includes/validation.js
@@ -4,12 +4,20 @@ import {
   defineRule,
   ErrorMessage,
 } from "vee-validate";
-import { required } from "@vee-validate/rules";
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+} from "@vee-validate/rules";
 export default {
   install(app) {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
     defineRule("required", required);
     app.component("ErrorMessage", ErrorMessage);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alpha_spaces", alphaSpaces);
   },
 };
