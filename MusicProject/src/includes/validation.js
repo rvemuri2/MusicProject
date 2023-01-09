5e4c567018e2d25b95a25a6f0bbc0f2cf3861af3
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-08 23:37:47 -0800

    Setting up Form Validation, validation components, registering plugins

---
+++ MusicProject/src/includes/validation.js
@@ -0,0 +1,7 @@
+import { Form as VeeForm, Field as VeeField } from "vee-validate";
+export default {
+  install(app) {
+    app.component("VeeForm", VeeForm);
+    app.component("VeeField", VeeField);
+  },
+};
--- MusicProject/src/main.js
+++ MusicProject/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import VeeValidatePlugin from "./includes/validation";
 import App from "./App.vue";
 import router from "./router";
 
@@ -11,5 +11,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(VeeValidatePlugin);
 app.mount("#app");
