bd5d7a6eb8e03ab3bd324bf3fdf5cc2458264b2f
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-09 01:12:45 -0800

    Rendering multiple error messages

--- MusicProject/src/components/Auth.vue
+++ MusicProject/src/components/Auth.vue
@@ -131,11 +131,20 @@
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
               <vee-field
-                type="password"
                 name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
+                :bails="false"
+                v-slot="{ field, errors }"
+              >
+                <input
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field"
+                  type="password"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
               <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
@@ -201,7 +210,7 @@ export default {
         name: "required|min:3|max:100|alpha_spaces",
         email: "required|min:3|max:100|email",
         age: "required|min_value: 18|max_value: 100",
-        password: "required|min:3|max:100",
+        password: "required|min:9|max:100|excluded:password",
         confirm_password: "required|confirmed:@password",
         country: "required|excluded:Antarctica",
         tos: "required",
