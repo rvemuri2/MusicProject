59572981477b411c9fd57063ed28d3bf5aeee022
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-10 01:19:32 -0800

    persisting the user authentication

--- MusicProject/src/App.vue
+++ MusicProject/src/App.vue
@@ -274,11 +274,22 @@
 <script>
 import AppHeader from "@/components/AppHeader.vue";
 import Auth from "@/components/Auth.vue";
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
+import { auth } from "./includes/firebase";
 export default {
   name: "App",
   components: {
     AppHeader,
     Auth,
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+  },
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true;
+    }
   },
 };
 </script>
