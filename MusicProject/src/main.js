d2b3c120949385cc4b71947ab0f08378fc8a77fe
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-10 01:15:39 -0800

    initializing firebase first

--- MusicProject/src/main.js
+++ MusicProject/src/main.js
@@ -6,10 +6,16 @@ import router from "./router";
 
 import "./assets/base.css";
 import "./assets/main.css";
-import "./includes/firebase";
-const app = createApp(App);
+import { auth } from "./includes/firebase";
 
-app.use(createPinia());
-app.use(router);
-app.use(VeeValidatePlugin);
-app.mount("#app");
+let app;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+
+    app.use(createPinia());
+    app.use(router);
+    app.use(VeeValidatePlugin);
+    app.mount("#app");
+  }
+});
