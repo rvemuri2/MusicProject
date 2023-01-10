ff1a26f35cd054bb4d9ab4ef34c48f3418d5c27f
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-09 22:47:26 -0800

    custom error messages and validation triggers

--- MusicProject/src/components/Auth.vue
+++ MusicProject/src/components/Auth.vue
@@ -212,9 +212,9 @@ export default {
         email: "required|min:3|max:100|email",
         age: "required|min_value: 18|max_value: 100",
         password: "required|min:9|max:100|excluded:password",
-        confirm_password: "required|confirmed:@password",
-        country: "required|excluded:Antarctica",
-        tos: "required",
+        confirm_password: "passwords_mismatch:@password",
+        country: "required|country_excluded:Antarctica",
+        tos: "tos",
       },
       userData: {
         country: "USA",
--- MusicProject/src/includes/validation.js
+++ MusicProject/src/includes/validation.js
@@ -3,6 +3,7 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from "vee-validate";
 import {
   required,
@@ -20,6 +21,7 @@ export default {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
     defineRule("required", required);
+    defineRule("tos", required);
     app.component("ErrorMessage", ErrorMessage);
     defineRule("min", min);
     defineRule("max", max);
@@ -27,7 +29,33 @@ export default {
     defineRule("email", email);
     defineRule("min_value", minVal);
     defineRule("max_value", maxVal);
-    defineRule("confirmed", confirmed);
+    defineRule("passwords_mismatch", confirmed);
     defineRule("excluded", excluded);
+    defineRule("country_excluded", excluded);
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `the field ${ctx.field} is required`,
+          min: `the field ${ctx.field} is too short`,
+          max: `the field ${ctx.field} is too long`,
+          alpha_spaces: `the field ${ctx.field} may only contain alphabetical characters and spaces`,
+          min_value: `the field ${ctx.field} is too low`,
+          max_value: `the field ${ctx.field} is too long`,
+          email: `the field ${ctx.field} is an invalid email`,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}`,
+          country_excluded: `We can't accept users from this location`,
+          passwords_mismatch: `The passwords don't match`,
+          tos: `You must accept the terms of service`,
+        };
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `the field ${ctx.field} is invalid`;
+        return message;
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
