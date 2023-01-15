8210ee523cad8a7e02a04f8efa77f27e52281c29
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-15 01:13:29 -0800

    creating links with hash fragments

--- MusicProject/src/views/Song.vue
+++ MusicProject/src/views/Song.vue
@@ -69,7 +69,7 @@
     </div>
   </section>
   <!-- Comments -->
-  <ul class="container mx-auto">
+  <ul class="container mx-auto" id="comments">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
       v-for="comment in sortedComments"
