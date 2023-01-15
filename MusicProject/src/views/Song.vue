6e829fe0a85e465fe6263cf94e705e61835c832e
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-14 23:18:49 -0800

    validating the comment

--- MusicProject/src/views/Song.vue
+++ MusicProject/src/views/Song.vue
@@ -29,20 +29,21 @@
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
-        <form>
+        <vee-form :validation-schema="schema">
           <vee-field
             name="comment"
             as="textarea"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
             placeholder="Your comment here..."
           ></vee-field>
+          <ErrorMessage class="text-red-600" name="comment" />
           <button
             type="submit"
             class="py-1.5 px-3 rounded text-white bg-green-600 block"
           >
             Submit
           </button>
-        </form>
+        </vee-form>
         <!-- Sort Comments -->
         <select
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
@@ -130,12 +131,16 @@
   </ul>
 </template>
 <script>
+import { ErrorMessage } from "vee-validate";
 import { songsCollection } from "../includes/firebase";
 export default {
   name: "Song",
   data() {
     return {
       song: {},
+      schema: {
+        comment: "required|min:3",
+      },
     };
   },
   async created() {
@@ -146,5 +151,6 @@ export default {
     }
     this.song = docSnapshot.data();
   },
+  components: { ErrorMessage },
 };
 </script>
